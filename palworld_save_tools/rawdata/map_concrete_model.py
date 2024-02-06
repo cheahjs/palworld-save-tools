eebc0962b79ac3dea861252470509332b020e42a
Jun Siang Cheah <me@jscheah.me>
2024-02-06 20:05:44 +0800

    remove commented code

--- palworld_save_tools/rawdata/map_concrete_model.py
+++ palworld_save_tools/rawdata/map_concrete_model.py
@@ -339,37 +339,6 @@ NO_OP_TYPES = set(
     ]
 )
 
-# def decode(
-#     reader: FArchiveReader, type_name: str, size: int, path: str
-# ) -> dict[str, Any]:
-#     if type_name != "StructProperty":
-#         raise Exception(f"Expected StructProperty, got {type_name}")
-#     value = reader.property(type_name, size, path, nested_caller_path=path)
-#     # Decode the raw bytes for the map object and replace the raw data
-#     raw_bytes = value["value"]["RawData"]["value"]["values"]
-#     print("".join(f"{b:02x}" for b in raw_bytes))
-#     value["value"]["RawData"]["value"] = decode_bytes(raw_bytes)
-#     # Decode the raw bytes for the module map and replace the raw data
-#     # group_map = value["value"]
-#     # for group in group_map:
-#     #     group_type = group["value"]["GroupType"]["value"]["value"]
-#     #     group_bytes = group["value"]["RawData"]["value"]["values"]
-#     #     group["value"]["RawData"]["value"] = decode_map_concrete_model_bytes(
-#     #         group_bytes, group_type
-#     #     )
-#     # EPalMapObjectConcreteModelModuleType::None = 0,
-#     # EPalMapObjectConcreteModelModuleType::ItemContainer = 1,
-#     # EPalMapObjectConcreteModelModuleType::CharacterContainer = 2,
-#     # EPalMapObjectConcreteModelModuleType::Workee = 3,
-#     # EPalMapObjectConcreteModelModuleType::Energy = 4,
-#     # EPalMapObjectConcreteModelModuleType::StatusObserver = 5,
-#     # EPalMapObjectConcreteModelModuleType::ItemStack = 6,
-#     # EPalMapObjectConcreteModelModuleType::Switch = 7,
-#     # EPalMapObjectConcreteModelModuleType::PlayerRecord = 8,
-#     # EPalMapObjectConcreteModelModuleType::BaseCampPassiveEffect = 9,
-#     # EPalMapObjectConcreteModelModuleType::PasswordLock = 10,
-#     return value
-
 
 def decode_bytes(
     parent_reader: FArchiveReader, m_bytes: Sequence[int], object_id: str
@@ -473,17 +442,6 @@ def decode_bytes(
             f"Warning: EOF not reached for {object_id} {map_object_concrete_model}: ori: {''.join(f'{b:02x}' for b in m_bytes)} remaining: {reader.size - reader.data.tell()}"
         )
     return data
-
-
-# def encode(
-#     writer: FArchiveWriter, property_type: str, properties: dict[str, Any]
-# ) -> int:
-#     if property_type != "MapProperty":
-#         raise Exception(f"Expected MapProperty, got {property_type}")
-#     del properties["custom_type"]
-#     # encoded_bytes = encode_map_concrete_model_bytes(properties["value"]["RawData"]["value"])
-#     # properties["value"]["RawData"]["value"] = {"values": [b for b in encoded_bytes]}
-#     return writer.property_inner(property_type, properties)
 
 
 def encode_bytes(p: Optional[dict[str, Any]]) -> bytes:
